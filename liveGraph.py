3d106cfd1a143c334b74bc0df7a7b0e3e2170787
Mekiiiii <mecialex@gmail.com>
2020-07-03 20:45:57 -0400

    Working Live Graph

--- liveGraph.py
+++ liveGraph.py
@@ -1,29 +1,19 @@
-from urllib.request import urlopen
-import time
-from PIL import Image 
 import datetime as dt
-import grequests
 import requests
 from bs4 import BeautifulSoup
-from itertools import count
-from operator import truediv
-import locale
 import json
 import pandas as pd
-from dhooks import Webhook, File
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.dates as mdates
 import matplotlib.animation as animation
-import mplfinance as mpf
 from mplfinance.original_flavor import candlestick_ohlc
-#import pandas_datareader.data as web
 import matplotlib
-from tiingo import TiingoClient
 import pylab
 import csv
 import os
+import sys
 
 
 
@@ -81,72 +71,68 @@ def computeMACD(x, slow=26, fast=12):
     return emaslow, emafast, emafast - emaslow
     
 def newData():
-    
     try:
         
         url = 'https://finance.yahoo.com/quote/' + stock
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'lxml')
         ticker = soup.find_all('div', {'class':'D(ib) Mt(-5px) Mend(20px) Maw(56%)--tab768 Maw(52%) Ov(h) smartphone_Maw(85%) smartphone_Mend(0px)'})[0].find('h1').text
-        close = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+        closep = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+        close = closep.replace(',', '')
         openp = soup.find_all('td', {'class': 'Ta(end) Fw(600) Lh(14px)'})
         openp = openp[1].find('span').text
+        openpp = openp.replace(',', '')
         high = soup.find_all('td', {'class': 'Ta(end) Fw(600) Lh(14px)'})
         high = high[4].text
         result = [x.strip() for x in high.split(' - ')]
         highp = result[1]
+        highpp = highp.replace(',', '')
         lowp = result[0]
-        #[float(i.replace(',','')) for i in array_one]
+        low = lowp.replace(',', '')
+        volumep = soup.find_all('td', {'class': 'Ta(end) Fw(600) Lh(14px)'})
+        volumep = volumep[6].find('span').text
+        volume = volumep.replace(',', '')
         tt = [x.strip() for x in ticker.split(' ')]
         tick = tt[0]
-                    #volume = soup.find_all('div', {'class': 'D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY smartphone_Bdc($seperatorColor)'})[7].find('span').text
+        
         print(tick)
         print(close)
-        print(openp)
-        print(highp)
-        print(lowp)
+        print(openpp)
+        print(highpp)
+        print(low)
+        print(volume)
         now = dt.datetime.now()
-        fieldnames = ["date","close","high","low","open"]
-        toFile(tick, close, now, highp, lowp, openp, fieldnames)
+        fieldnames = ["date","close","high","low","open", "volume"]
+        toFile(tick, close, now, highpp, low, openpp, volume, fieldnames)
     except IndexError as e:
         os.execv(sys.executable, ['python3'] + sys.argv)
         print(e)
 
-def toFile(ticker, price_data, time, high, low, openn, fieldnames):
-
-    x_value = time
-    close = price_data
-
-    
+def toFile(ticker, price_data, time, high, low, openn, volume, fieldnames):  
     with open('liveGraphfiles/' + ticker + '.csv', 'a') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         if csv_file.tell() == 0:
             csv_writer.writeheader()
 
         info = {
-            "date": x_value,
+            "date": time,
             "close": price_data,
             "high": high,
             "low": low,
-            "open": openn
+            "open": openn,
+            "volume": volume
                     }
 
         csv_writer.writerow(info)
-        # print(x_value, tick, curr_price)
 
 def graphData(stock, MA1, MA2):
     fig.clf()
-    '''
-        Use this to dynamically pull a stock:
-    '''
-        
     try:
         df = pd.read_csv('liveGraphfiles/' + stock + '.csv')
         df.index = pd.to_datetime(df.index)
         df.rename(columns={'date': 'Date', 'close': 'Close', 'open': 'Open', 'high': 'High', 'low': 'Low', 'volume': 'Volume'}, inplace=True)
         df ['Date'] = df['Date'].map(lambda x: str(x)[:-15])
         df.index.name = 'Date'
-        #df = df[(df['Date'] > '2018-1-1') & (df['Date'] <= '2020-8-20')]
         df['Date'] = pd.to_datetime(df['Date'])
         df['Date'] = df['Date'].apply(mdates.date2num)
         df = df.astype(float)
@@ -155,8 +141,7 @@ def graphData(stock, MA1, MA2):
         highp = df['High']
         lowp = df['Low']
         openp = df['Open']
-        #print(date)
-        #volume = df['Volume']
+        volume = df['Volume']
 
         rsi = rsiFunc(closep)
         x = 0
@@ -166,27 +151,23 @@ def graphData(stock, MA1, MA2):
             appendLine = date[x], openp[x], highp[x], lowp[x], closep[x]
             newAr.append(appendLine)
             x += 1
-        #print(newAr)
+
         Av1 = movingaverage(closep, MA1)
         Av2 = movingaverage(closep, MA2)
 
         SP = len(date[MA2-1:])
-        #print(SP)
-
-        
 
         ax1 = plt.subplot2grid(
             (6, 4), (1, 0), rowspan=4, colspan=4, facecolor='#07000d')
-        #print(date)
+
         candlestick_ohlc(ax1, newAr[-SP:], colorup='green', colordown='red', width=0.6, alpha=1.0) 
     
         Label1 = str(MA1)+' SMA'
         Label2 = str(MA2)+' SMA'
         
-        print(date)
-        print(Av1)
+        # print(date)
+        # print(Av1)
 
-        #mpf.plot(df, type='candle', style='mike')
         ax1.plot(date[-SP:], Av1[-SP:], '#FFFF00',
                     label=Label1, linewidth=1.5)
         ax1.plot(date[-SP:], Av2[-SP:], '#4ee6fd',
@@ -212,11 +193,10 @@ def graphData(stock, MA1, MA2):
         textEd = pylab.gca().get_legend().get_texts()
         pylab.setp(textEd[0:5], color='w')
 
-        # volumeMin = 0
+        volumeMin = 0
 
         ax0 = plt.subplot2grid(
             (6, 4), (0, 0), sharex=ax1, rowspan=1, colspan=4, facecolor='#07000d')
-        # rsi = rsiFunc(closep)
         rsiCol = '#c1f9f7'
         posCol = '#386d13'
         negCol = '#8f2020'
@@ -236,18 +216,18 @@ def graphData(stock, MA1, MA2):
         ax0.tick_params(axis='x', colors='w')
         plt.ylabel('RSI')
 
-        # ax1v = ax1.twinx()
-        # ax1v.fill_between(date[-SP:], volumeMin, volume[-SP:], facecolor='#00ffe8', alpha=.4)
-        # ax1v.axes.yaxis.set_ticklabels([])
-        # ax1v.grid(False)
-        # # Edit this to 3, so it's a bit larger
-        # ax1v.set_ylim(0, 3*volume.max())
-        # ax1v.spines['bottom'].set_color("#5998ff")
-        # ax1v.spines['top'].set_color("#5998ff")
-        # ax1v.spines['left'].set_color("#5998ff")
-        # ax1v.spines['right'].set_color("#5998ff")
-        # ax1v.tick_params(axis='x', colors='w')
-        # ax1v.tick_params(axis='y', colors='w')
+        ax1v = ax1.twinx()
+        ax1v.fill_between(date[-SP:], volumeMin, volume[-SP:], facecolor='#00ffe8', alpha=.4)
+        ax1v.axes.yaxis.set_ticklabels([])
+        ax1v.grid(False)
+        # Edit this to 3, so it's a bit larger
+        ax1v.set_ylim(0, 3*volume.max())
+        ax1v.spines['bottom'].set_color("#5998ff")
+        ax1v.spines['top'].set_color("#5998ff")
+        ax1v.spines['left'].set_color("#5998ff")
+        ax1v.spines['right'].set_color("#5998ff")
+        ax1v.tick_params(axis='x', colors='w')
+        ax1v.tick_params(axis='y', colors='w')
         ax2 = plt.subplot2grid(
             (6, 4), (5, 0), sharex=ax1, rowspan=1, colspan=4, facecolor='#07000d')
         fillcolor = '#00ffe8'
@@ -292,19 +272,15 @@ def graphData(stock, MA1, MA2):
         print('main loop', str(e))
 
 
-# newData()
-# for n in range(length):
-#     word = ticker_array[n]
-#     graphData(word,10,50)
+
 fig = plt.figure(facecolor='#07000d')
 def animate(i):
     newData()
     graphData(stock,10,50)
+
 global stock
 stock = ('SQQQ')
-#newData()
-while True:
-    
+while True:  
     f = open('liveGraphfiles/' + stock + '.csv')
     csv_f = csv.reader(f)
     row_count = sum(1 for row in csv_f)
